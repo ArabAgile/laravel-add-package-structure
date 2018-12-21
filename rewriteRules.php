19f614b999569cc378268bae656bad247daf72d7
AmmarR <a.alrefai@hbmsu.ac.ae>
2018-12-22 00:53:05 +0400

    include controllers

--- rewriteRules.php
+++ rewriteRules.php
@@ -4,6 +4,6 @@
 return [
     'src/PkgServiceProvider.php' => 'src/:uc:packageServiceProvider.php',
     'src/Models/Pkg.php' => 'src/Models/:uc:package.php',
-    'src/Controllers/ApiController.php' => 'src/Controllers/ApiController.php',
-    'src/Controllers/WebController.php' => 'src/Controllers/WebController.php',
+    'src/Controllers/ApiController.php' => 'src/Controllers/:uc:packageApiController.php',
+    'src/Controllers/WebController.php' => 'src/Controllers/:uc:packageWebController.php',
 ];
